679a6bf7ad9005079229a2017ea2000f92bee6f5
davidespihernandez <david.esp.hernndez@clevertech.biz>
2017-10-30 10:57:03 +0100

    Detail, dates

--- src/main/java/com/despi/jwtrestserver/domain/StampedEntity.java
+++ src/main/java/com/despi/jwtrestserver/domain/StampedEntity.java
@@ -3,36 +3,50 @@ package com.despi.jwtrestserver.domain;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 
 @MappedSuperclass
 abstract public class StampedEntity {
-	private ZonedDateTime createdDate;
-	private ZonedDateTime modifiedDate;
+	public static final ZoneOffset DEFAULT_ZONE_ID = ZoneOffset.UTC;
+
+	// @CreationTimestamp and @UpdateTimestamp are VM timezone dependent, prefer this agnostic approach
+	protected Timestamp createdDate;
+	protected Timestamp modifiedDate;
 
 	@PrePersist
 	public void autoCreatedTracking() {
-		createdDate = modifiedDate = ZonedDateTime.now();
+		createdDate = modifiedDate = Timestamp.valueOf(LocalDateTime.now(DEFAULT_ZONE_ID));
 	}
 
 	@PreUpdate
 	public void autoModifiedTracking() {
-		modifiedDate = ZonedDateTime.now();
+		modifiedDate = Timestamp.valueOf(LocalDateTime.now(DEFAULT_ZONE_ID));
 	}
 
-	public ZonedDateTime getCreatedDate() {
+	public ZonedDateTime getCreatedTS() {
+		return createdDate != null ? ZonedDateTime.of(createdDate.toLocalDateTime(), DEFAULT_ZONE_ID) : null;
+	}
+
+	public ZonedDateTime getModifiedTS() {
+		return modifiedDate != null ? ZonedDateTime.of(modifiedDate.toLocalDateTime(), DEFAULT_ZONE_ID) : null;
+	}
+
+	public Timestamp getCreatedDate() {
 		return createdDate;
 	}
 
-	public void setCreatedDate(ZonedDateTime createdDate) {
+	public void setCreatedDate(Timestamp createdDate) {
 		this.createdDate = createdDate;
 	}
 
-	public ZonedDateTime getModifiedDate() {
+	public Timestamp getModifiedDate() {
 		return modifiedDate;
 	}
 
-	public void setModifiedDate(ZonedDateTime modifiedDate) {
+	public void setModifiedDate(Timestamp modifiedDate) {
 		this.modifiedDate = modifiedDate;
 	}
 }
